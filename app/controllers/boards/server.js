48fdeb6705159851ee082cca721c8711eecbbca4
okay <okay@chert>
2014-03-14 09:50:15 -0700

    adding in the basic post component

--- app/controllers/boards/server.js
+++ app/controllers/boards/server.js
@@ -6,6 +6,7 @@ var value_of = controller.value_of,
     array_of = controller.array_of;
 
 var Post = require_app("models/post");
+var $ = require("cheerio");
 
 module.exports = {
   // If the controller has assets in its subdirs, set is_package to true
@@ -16,11 +17,30 @@ module.exports = {
 
   index: function(ctx, api) {
     var board_id = ctx.req.params.id;
-    var template_str = api.template.render("controllers/boards/boards.html.erb", { board: board_id});
-    Post.findAll({ where: { board_id: board_id }}).success(function(results) {
-      console.log("POSTS ARE", results); 
+
+    var render_posts = api.page.async(function(flush) {
+      Post.findAll({ where: { board_id: board_id }})
+        .success(function(results) {
+          var div = $("<div />");
+          _.each(results, function(result) {
+            delete result.dataValues.id;
+            var postCmp = $C("post", result.dataValues );
+            div.append(postCmp.$el);
+            postCmp.marshall();
+          });
+          flush(div);
+        });
     });
-    api.page.render({ content: template_str});
+
+    var template_str = api.template.render("controllers/boards/boards.html.erb", {
+      board: board_id,
+      render_posts: render_posts
+    });
+
+    api.page.render({ 
+      content: template_str
+    });
+
   },
 
   socket: function() {}
