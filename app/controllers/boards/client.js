f3a01a79ab67ba4e1ca45dce64837880bc0e2b58
okay <okay@chert>
2014-03-18 18:27:25 -0700

    no need for chromahash when it isnt right

--- app/controllers/boards/client.js
+++ app/controllers/boards/client.js
@@ -1,14 +1,26 @@
 "use strict";
 
 require("core/client/component");
+var tripcode_gen = require("app/client/tripcode");
+
 
 module.exports = {
   events: {
     "submit form.new_post" : "add_post",
     "change input.tripcode" : "save_tripcode",
     "change input.handle" : "save_handle",
+    "keyup input.tripcode" : "update_trip_colors",
+    "keyup input.handle" : "update_trip_colors",
     "change input.newtrip" : "save_newtrip"
   },
+  update_trip_colors: _.throttle(function() {
+    if (SF.controller().gen_tripcode) {
+      var tripcodeHash = this.$el.find(".identity_tripcode");
+      tripcodeHash.empty();
+      tripcodeHash.data("tripcode", this.get_trip_identity());
+      SF.controller().gen_tripcode(tripcodeHash);
+    }
+  }, 100),
   save_newtrip: function() {
     var newtripEl = this.$page.find("input.newtrip");
     var newtrip = !!newtripEl.prop('checked');
@@ -18,14 +30,17 @@ module.exports = {
     var tripcodeEl = this.$page.find("input.tripcode");
     var tripcode = tripcodeEl.val();
     this.save_newtrip();
+    this.update_trip_colors();
     $.cookie("tripcode", tripcode);
   },
   save_handle: function() {
     var handleEl = this.$page.find("input.handle");
     var handle = handleEl.val();
     this.save_newtrip();
+    this.update_trip_colors();
     $.cookie("handle", handle);
   },
+  gen_tripcode: tripcode_gen,
   add_post: function(e) {
     e.preventDefault();
 
@@ -54,7 +69,6 @@ module.exports = {
     var handleEl = this.$page.find("input.handle");
     var newtripEl = this.$page.find("input.newtrip");
     var newtrip = $.cookie("newtrip") === "true";
-    tripcodeEl.chromaHash({bars: 4});
     var tripcode = $.cookie("tripcode");
     var handle = $.cookie("handle");
 
@@ -78,6 +92,9 @@ module.exports = {
   },
   get_tripcode: function() {
     return md5($("input.tripcode").val());
+  },
+  get_trip_identity: function() {
+    return md5(this.get_handle() + ":" + this.get_tripcode());
   },
   get_handle: function() {
     return $("input.handle").val();
--- components/post/post.js
+++ components/post/post.js
@@ -1,49 +1,5 @@
 "use strict";
 
-// http://stackoverflow.com/a/10075654/442652
-function padDigits(number, digits) {
-  return Array(Math.max(digits - String(number).length + 1, 0)).join(0) + number;
-}
-
-function get_colors_for_hash(hashed) {
-  hashed = hashed || window.md5(hashed);
-  var colors = hashed.match(/([\dABCDEF]{6})/ig);
-
-  var hexes = [];
-  for (var i = 0; i < 4; i++) {
-    var color = parseInt(colors[i], 16);
-    var red = (color >> 16) & 255;
-    var green = (color >> 8) & 255;
-    var blue = color & 255;
-    var hex = $.map([red, green, blue], function(c, i) {
-      return padDigits(((c >> 4) * 0x10).toString(16), 2);
-    }).join('');
-
-    hexes.push(hex);
-  }
-
-  return hexes;
-}
-
-function gen_tripcode(el) {
-  // Now that we have our tripcodes, do other things...
-  var colors = get_colors_for_hash($(el).data("tripcode"));
-  var div = $(el);
-  _.each(colors, function(color) {
-    var colorDiv = $("<div />").css({
-      backgroundColor: "#" + color,
-      display: "inline-block",
-      height: "20px",
-      width: "25%"
-    });
-    div.append(colorDiv);
-
-    $(el).css({
-      position: "relative"
-    });
-  });
-}
-
 function add_icons($el) {
   var escaped = $el.text();
   if (escaped) {
@@ -84,7 +40,7 @@ module.exports = {
     this.$el.find(".timeago").timeago();
     this.$el.find(".post").fadeIn();
     this.$el.find("div.tripcode").each(function() {
-      gen_tripcode(this);
+      SF.controller().gen_tripcode(this);
     });
   },
   add_reply: function(data) {
