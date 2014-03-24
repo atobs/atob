e356f5ec130c06aa943355d04f6a439b8fcc8086
okay <okay@chert>
2014-03-24 00:28:51 -0700

    bump max post length to 500

--- app/client/text.js
+++ app/client/text.js
@@ -35,7 +35,7 @@ function add_replies($el) {
 
 function shorten_text($el) {
   var escaped = $el.html();
-  if (escaped.length > 300) {
+  if (escaped.length > 500) {
     $el.addClass("hideContent");
     $el.addClass("truncable");
 
