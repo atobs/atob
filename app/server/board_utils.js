94f841f0b8966067242fef24b48768ce429a9b2f
okay <okay@chert>
2014-05-31 21:23:06 -0700

    hardcoding board names and their order

--- app/server/board_utils.js
+++ app/server/board_utils.js
@@ -5,22 +5,13 @@ var Board = require_app("models/board");
 module.exports = {
   render_boards: function() {
     var render_boards = page.async(function(flush) {
-      Board.findAll({
-          order: "name ASC"
-        })
-        .success(function(results) {
-          var boards = _.map(results, function(r) {
-            return r.getDataValue('name');
-          });
+      var boards = [ "a", "to", "b", "links", "gifs" ];
 
-          var template_str = template.partial("home/board_links.html.erb", {
-            boards: boards
-          });
+      var template_str = template.partial("home/board_links.html.erb", {
+        boards: boards
+      });
 
-          flush(template_str);
-
-        });
-
+      flush(template_str);
 
     });
 
