0cab065cb895d4a89ad9af1c438661643a2bd5e1
okay <okay@chert>
2015-06-02 10:25:51 -0700

    burtle storms become harder to achieve as time goes on

--- app/server/makeme_store.js
+++ app/server/makeme_store.js
@@ -370,6 +370,7 @@ module.exports = {
       posts_controller.get_socket().emit("burtledance");
 
       console.log("METER MAXED OUT!");
+      METER_MAX *= 1.5;
     }
     METER_TOTAL %= METER_MAX;
 
